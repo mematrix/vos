1b56bb7f7f970baa780d5aee221bc07cdf2cdaea
mematrix <mematrix@users.noreply.github.com>
2022-09-05 22:11:48 +0800

    Add MMU operations: add Sv48, Sv57 modes impl

--- src/mem/mmu.rs
+++ src/mem/mmu.rs
@@ -7,7 +7,8 @@
 //! identify PTE is set to cover the PageEntry physical memory. Because read and
 //! write operation of the PTE all access the physical memory directly.
 
-use crate::mem::page::{PAGE_ORDER, zalloc};
+use core::ptr::null_mut;
+use crate::mem::page::{PAGE_ORDER, PAGE_SIZE, alloc, zalloc, dealloc};
 
 
 #[repr(u32)]
@@ -137,6 +138,18 @@ impl Entry {
         !self.is_leaf()
     }
 
+    /// Check if the **Access** bit is set.
+    #[inline]
+    pub fn is_access_set(&self) -> bool {
+        self.entry & EntryBits::Access.val_u64() != 0
+    }
+
+    /// Check if the **Access** bit is clear.
+    #[inline]
+    pub fn is_access_clear(&self) -> bool {
+        !self.is_access_set()
+    }
+
     #[inline]
     pub fn set_entry(&mut self, entry: u64) {
         self.entry = entry;
@@ -222,13 +235,60 @@ pub trait Table {
     unsafe fn destroy(&mut self);
 }
 
+fn cast_to_table<T: Table + 'static>() -> *mut dyn Table {
+    let page = zalloc(1);
+    if page == 0 {
+        null_mut::<T>() as *mut dyn Table
+    } else {
+        page as *mut T as *mut dyn Table
+    }
+}
+
 /// Create a root table with the special `mode`. Return a trait object pointer that
 /// holds all implementations for the input mode.
 ///
 /// **Call Convention**: This function **must** be called from the M-mode or in the
 /// S-mode with suitable identify PTEs are set.
 pub fn create_root_table(mode: Mode) -> *mut dyn Table {
-    //
+    match mode {
+        Mode::Bare => {
+            &mut BareTable as *mut dyn Table
+        }
+        Mode::Sv39 => {
+            cast_to_table::<Sv39Table>()
+        }
+        Mode::Sv48 => {
+            cast_to_table::<Sv48Table>()
+        }
+        Mode::Sv57 => {
+            cast_to_table::<Sv57Table>()
+        }
+    }
+}
+
+/// Copy the root table content. If any entry of the root table is a branch, the
+/// sub-table will not be copied, and the new table will refer to the same sub-level
+/// tables.
+///
+/// **Call Convention**: This function **must** be called from the M-mode or in the
+/// S-mode with suitable identify PTEs are set.
+pub fn copy_root_table(root: &dyn Table) -> *mut dyn Table {
+    let pt_addr = alloc(1);
+    let addr = root.get_addr();
+    // Page table for each modes has the same size that equals to `PAGE_SIZE`,
+    // just copy with ignoring the underlying format.
+    let pt_ptr = pt_addr as *mut u64;
+    let ptr = addr as *const u64;
+    for i in 0..PAGE_SIZE / 8 {
+        // Force a ld and sd instruction.
+        unsafe {
+            *pt_ptr.add(i) = *ptr.add(i);
+        }
+    }
+
+    unsafe {
+        build_table_from_addr(pt_addr, root.get_mode())
+    }
 }
 
 /// Build a `Table` trait object pointer from the page table physical address and
@@ -242,7 +302,20 @@ pub fn create_root_table(mode: Mode) -> *mut dyn Table {
 /// The caller **must** make sure that the `mode` is correctly matching the page
 /// table address.
 pub unsafe fn build_table_from_addr(addr: usize, mode: Mode) -> *mut dyn Table {
-    //
+    match mode {
+        Mode::Bare => {
+            &mut BareTable as *mut dyn Table
+        }
+        Mode::Sv39 => {
+            addr as *mut Sv39Table as *mut dyn Table
+        }
+        Mode::Sv48 => {
+            addr as *mut Sv48Table as *mut dyn Table
+        }
+        Mode::Sv57 => {
+            addr as *mut Sv57Table as *mut dyn Table
+        }
+    }
 }
 
 
@@ -255,35 +328,35 @@ const PTE_FLAG_MASK: u64 = 0x3ff;
 const PTE_PPN_MASK: u64 = !0x3ff;
 
 /// Common map function for Sv39, Sv48, Sv57 mode.
-fn do_map<const LEVELS: u32, const PTE_SIZE: usize, const AUTO_VALID: bool>(
+fn do_map<const LEVELS: usize, const PTE_SIZE: usize, const AUTO_VALID: bool>(
     root: usize,
     v_addr: usize, p_addr: usize,
     bits: u32, level: u32) {
-    debug_assert!(level < LEVELS);
+    debug_assert!(level < LEVELS as u32);
     // The virtual address and physical address should align to the corresponding
     // page size.
-    debug_assert!(v_addr & ((1usize << (level * 9 + PAGE_ORDER)) - 1) == 0);
-    debug_assert!(p_addr & ((1usize << (level * 9 + PAGE_ORDER)) - 1) == 0);
+    debug_assert!(v_addr & ((1usize << (level * 9 + PAGE_ORDER as u32)) - 1) == 0);
+    debug_assert!(p_addr & ((1usize << (level * 9 + PAGE_ORDER as u32)) - 1) == 0);
 
     // Make sure the RWX bits are set and valid.
     assert!(is_bits_valid_leaf(bits));
 
     // Top PPN value need special mask.
-    const L_PPN_MASK: usize = (1usize << (44usize - (LEVELS - 1) * 9)) - 1;
+    let ppn_mask = (1usize << (44usize - (LEVELS - 1) * 9)) - 1;
 
     // On Sv39, Sv48, Sv57 modes, each VPN is exactly 9 bits; PPN[LEVELS-2:0] is also
     // exactly 9 bits while PPN[LEVELS-1] is (44-(LEVELS-1)*9) bits.
     // We init the `ppn` with 0 to satisfy the align requirement when `level` is not 0.
-    let ppn = [0usize; LEVELS];
+    let mut ppn = [0usize; LEVELS];
     // Read top level VPN.
     let vpn = (v_addr >> ((LEVELS - 1) * 9 + PAGE_ORDER)) & L_MASK;
-    ppn[LEVELS - 1] = (p_addr >> ((LEVELS - 1) * 9 + PAGE_ORDER)) & L_PPN_MASK;
+    ppn[LEVELS - 1] = (p_addr >> ((LEVELS - 1) * 9 + PAGE_ORDER)) & ppn_mask;
 
     // Read the first PTE.
     let v = (root + vpn * PTE_SIZE) as *mut Entry;
     let mut v = unsafe { &mut *v };
     // Traverse the page table.
-    for i in (level..LEVELS - 1).rev() {
+    for i in (level as usize..LEVELS - 1).rev() {
         if v.is_invalid() {
             // Alloc a page.
             let page = zalloc(1);
@@ -304,14 +377,14 @@ fn do_map<const LEVELS: u32, const PTE_SIZE: usize, const AUTO_VALID: bool>(
     let mut entry = (bits as u64 & PTE_FLAG_MASK)
         | (AUTO_VALID as u64 & EntryBits::Valid.val_u64());
     for (i, p) in ppn.iter().enumerate() {
-        entry |= p << (i * 9 + 10);
+        entry |= (p << (i * 9 + 10)) as u64;
     }
     v.set_entry(entry);
 }
 
 /// Common unmap function for Sv39, Sv48, Sv57 modes.
 /// If the PTE is changed, return true, otherwise return false.
-fn do_unmap<const LEVELS: u32>(root: usize, v_addr: usize) -> bool {
+fn do_unmap<const LEVELS: usize>(root: usize, v_addr: usize) -> bool {
     let mut entry = root as *mut Entry;
 
     for i in (0..LEVELS).rev() {
@@ -341,15 +414,134 @@ fn do_unmap<const LEVELS: u32>(root: usize, v_addr: usize) -> bool {
 ///
 /// The algorithm for virtual-to-physical address translation is described in
 /// RISC-V Privileged Spec Section 4.3.2.
-fn do_virt2phys<const LEVELS: u32>(root: usize, v_addr: usize) -> Option<usize> {
-    //
+fn do_virt2phys<const LEVELS: usize>(root: usize, v_addr: usize) -> Option<usize> {
+    let mut entry = root as *mut Entry;
+
+    for i in (0..LEVELS).rev() {
+        let shift = i * 9 + PAGE_ORDER;
+        let vpn = (v_addr >> shift) & L_MASK;
+        let v = unsafe { &mut *entry.add(vpn) };
+        // We here only check the `Valid` bit, other flag bits should be checked
+        // when do map operation.
+        if v.is_invalid() {
+            break;
+        }
+        if v.is_leaf() {
+            // If the page is not in the physical memory (for example, swapped to
+            // the disk), then the `Access` bit is clear, and the entry's ppn is
+            // the ID of disk content.
+            if v.is_access_clear() {
+                break;
+            }
+
+            // Offset mask to read PPN prefix and v-addr offset part.
+            let mask = (1usize << shift) - 1usize;
+            let va_offset = v_addr & mask;
+            let pn = ((v.get_entry() << 2) as usize) & !mask;
+            return Some(pn | va_offset);
+        }
+
+        // Branch, read next.
+        entry = ((v.get_entry() & PTE_PPN_MASK) << 2) as *mut Entry;
+    }
+
+    None
+}
+
+fn leaf_table_is_used(addr: usize) -> bool {
+    let ptr = addr as *const u64;
+    let mut valid = 0u64;
+    for i in 0..ENTRIES_LEN {
+        valid |= unsafe { *ptr.add(i) };
+    }
+
+    valid & EntryBits::Valid.val_u64() != 0
+}
+
+fn walk_and_free_unused(addr: usize, level: u32, max_level: u32) -> (bool, bool) {
+    if level >= max_level {
+        return (leaf_table_is_used(addr), false);
+    }
+
+    let ptr = addr as *mut Entry;
+    let mut valid = 0u64;
+    let mut update = false;
+    for i in 0..ENTRIES_LEN {
+        let v = unsafe { &mut *ptr.add(i) };
+        if v.is_invalid() {
+            continue;
+        }
+
+        if v.is_leaf() {
+            valid |= v.get_entry();
+        } else {
+            let e = ((v.get_entry() & PTE_PPN_MASK) << 2) as usize;
+            let (b_v, b_u) = walk_and_free_unused(e, level + 1, max_level);
+            if b_v {
+                // Sub level table has at least a valid entry.
+                valid |= 0x1u64;
+                update |= b_u;
+            } else {
+                // All entries of sub level table have unmapped.
+                dealloc(e);
+                v.set_entry(0);
+                update = true;
+                // `valid |= 0x0u64` has no effect.
+            }
+        }
+    }
+
+    (valid & EntryBits::Valid.val_u64() != 0, update)
 }
 
 /// Common operation to walk and free the unused *branch* entry. Because all the
 /// modes supported by RV64 have the same length PTE, the scan process can be
 /// unified.
-fn do_free_unused_entry<const LEVELS: u32>(root: usize) -> bool {
-    //
+///
+/// If any sub-level table page was free, the *branch* entry will be clear. Returns
+/// `true` if any PTE has been changed, otherwise returns `false`.
+///
+/// **Note**: The root table will not be free.
+fn do_free_unused_entry<const LEVELS: usize>(root: usize) -> bool {
+    let entry = root as *mut Entry;
+
+    let mut update = false;
+    for i in 0..ENTRIES_LEN {
+        let v = unsafe { &mut *entry.add(i) };
+        if v.is_valid() && v.is_branch() {
+            let addr = ((v.get_entry() & PTE_PPN_MASK) << 2) as usize;
+            let (valid, u) = walk_and_free_unused(addr, 2, LEVELS as u32);
+            if valid {
+                update |= u;
+            } else {
+                // All entries of sub level table have been unmapped.
+                dealloc(addr);
+                v.set_entry(0);
+                update = true;
+            }
+        }
+    }
+
+    update
+}
+
+/// Recursive destroy the page table.
+///
+/// **Note**: Current table (`addr`) will also be destroyed.
+fn do_destroy(addr: usize, level: u32, max_level: u32) {
+    if level < max_level {
+        let entry = addr as *const Entry;
+
+        for i in 0..ENTRIES_LEN {
+            let v = unsafe { &*entry.add(i) };
+            if v.is_valid() && v.is_branch() {
+                let child = (v.get_entry() & PTE_PPN_MASK) << 2;
+                do_destroy(child as usize, level + 1, max_level);
+            }
+        }
+    }
+
+    dealloc(addr);
 }
 
 const ENTRIES_LEN: usize = 512;
@@ -360,11 +552,7 @@ struct Sv39Table {
 }
 
 impl Sv39Table {
-    const LEVELS: u32 = 3;
-
-    pub const fn len() -> usize {
-        ENTRIES_LEN
-    }
+    const LEVELS: usize = 3;
 }
 
 impl Table for Sv39Table {
@@ -377,29 +565,129 @@ impl Table for Sv39Table {
     }
 
     fn map(&mut self, v_addr: usize, p_addr: usize, bits: u32, level: u32) {
-        do_map::<Sv39Table::LEVELS, 8, true>(self.get_addr(), v_addr, p_addr, bits, level);
+        do_map::<{ Sv39Table::LEVELS }, 8, true>(self.get_addr(), v_addr, p_addr, bits, level);
     }
 
     fn unmap(&mut self, v_addr: usize) -> bool {
-        do_unmap::<Sv39Table::LEVELS>(self.get_addr(), v_addr)
+        do_unmap::<{ Sv39Table::LEVELS }>(self.get_addr(), v_addr)
     }
 
     fn virt_to_phys(&self, v_addr: usize) -> Option<usize> {
-        do_virt2phys::<Sv39Table::LEVELS>(self.get_addr(), v_addr)
+        do_virt2phys::<{ Sv39Table::LEVELS }>(self.get_addr(), v_addr)
     }
 
     fn free_unused_entry(&mut self) -> bool {
-        do_free_unused_entry::<Sv39Table::LEVELS>(self.get_addr())
+        do_free_unused_entry::<{ Sv39Table::LEVELS }>(self.get_addr())
     }
 
     unsafe fn destroy(&mut self) {
-        todo!()
+        do_destroy(self.get_addr(), 1, Sv39Table::LEVELS as u32);
     }
 }
 
+#[repr(C)]
+struct Sv48Table {
+    entries: [Entry; ENTRIES_LEN],
+}
 
+impl Sv48Table {
+    const LEVELS: usize = 4;
+}
 
+impl Table for Sv48Table {
+    fn get_addr(&self) -> usize {
+        self as *const Sv48Table as usize
+    }
 
+    fn get_mode(&self) -> Mode {
+        Mode::Sv48
+    }
 
+    fn map(&mut self, v_addr: usize, p_addr: usize, bits: u32, level: u32) {
+        do_map::<{ Sv48Table::LEVELS }, 8, true>(self.get_addr(), v_addr, p_addr, bits, level);
+    }
 
+    fn unmap(&mut self, v_addr: usize) -> bool {
+        do_unmap::<{ Sv48Table::LEVELS }>(self.get_addr(), v_addr)
+    }
 
+    fn virt_to_phys(&self, v_addr: usize) -> Option<usize> {
+        do_virt2phys::<{ Sv48Table::LEVELS }>(self.get_addr(), v_addr)
+    }
+
+    fn free_unused_entry(&mut self) -> bool {
+        do_free_unused_entry::<{ Sv48Table::LEVELS }>(self.get_addr())
+    }
+
+    unsafe fn destroy(&mut self) {
+        do_destroy(self.get_addr(), 1, Sv48Table::LEVELS as u32);
+    }
+}
+
+#[repr(C)]
+struct Sv57Table {
+    entries: [Entry; ENTRIES_LEN],
+}
+
+impl Sv57Table {
+    const LEVELS: usize = 5;
+}
+
+impl Table for Sv57Table {
+    fn get_addr(&self) -> usize {
+        self as *const Sv57Table as usize
+    }
+
+    fn get_mode(&self) -> Mode {
+        Mode::Sv57
+    }
+
+    fn map(&mut self, v_addr: usize, p_addr: usize, bits: u32, level: u32) {
+        do_map::<{ Sv57Table::LEVELS }, 8, true>(self.get_addr(), v_addr, p_addr, bits, level);
+    }
+
+    fn unmap(&mut self, v_addr: usize) -> bool {
+        do_unmap::<{ Sv57Table::LEVELS }>(self.get_addr(), v_addr)
+    }
+
+    fn virt_to_phys(&self, v_addr: usize) -> Option<usize> {
+        do_virt2phys::<{ Sv57Table::LEVELS }>(self.get_addr(), v_addr)
+    }
+
+    fn free_unused_entry(&mut self) -> bool {
+        do_free_unused_entry::<{ Sv57Table::LEVELS }>(self.get_addr())
+    }
+
+    unsafe fn destroy(&mut self) {
+        do_destroy(self.get_addr(), 1, Sv57Table::LEVELS as u32);
+    }
+}
+
+/// Mock table handles the **Bare** mode.
+struct BareTable;
+
+impl Table for BareTable {
+    fn get_addr(&self) -> usize {
+        0
+    }
+
+    fn get_mode(&self) -> Mode {
+        Mode::Bare
+    }
+
+    fn map(&mut self, _v_addr: usize, _p_addr: usize, _bits: u32, _level: u32) {}
+
+    fn unmap(&mut self, _v_addr: usize) -> bool {
+        false
+    }
+
+    fn virt_to_phys(&self, v_addr: usize) -> Option<usize> {
+        Some(v_addr)
+    }
+
+    fn free_unused_entry(&mut self) -> bool {
+        false
+    }
+
+    unsafe fn destroy(&mut self) {}
+}
