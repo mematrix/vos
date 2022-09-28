92b000d8ca2ff4ebaa8e1abdde411e53ba65bb80
mematrix <mematrix@users.noreply.github.com>
2022-09-28 13:51:15 +0800

    Update document and add `inline` attr.

--- src/util/align.rs
+++ src/util/align.rs
@@ -5,6 +5,12 @@ use core::mem::align_of;
 
 /// Get the order of an alignment value. Which is the count of the trailing zero bits
 /// of the `alignment`.
+///
+/// **Note**: The `alignment` value **must** be a power of 2, otherwise the result is
+/// incorrect. Type align info can be retrieved by [`core::mem::align_of`].
+///
+/// [`core::mem::align_of`]: ::core::mem::align_of
+#[inline(always)]
 pub const fn get_order(alignment: usize) -> usize {
     alignment.trailing_zeros() as usize
 }
@@ -17,16 +23,18 @@ pub const fn get_order(alignment: usize) -> usize {
 ///
 /// This function always rounds up. So the returned value will always be
 /// **not less than** the `val`.
+#[inline(always)]
 pub const fn align_up(val: usize, order: usize) -> usize {
     let o = (1usize << order) - 1;
     (val + o) & !o
 }
 
-/// Returns the **aligned** value of `val`. Similar to [`align_val_up`], but this
+/// Returns the **aligned** value of `val`. Similar to [`align_up`], but this
 /// function aligns value by rounding down, it will simple set the least `order` bits
 /// to zero. So the returned value will always be **not greater than** the `val`.
 ///
-/// [`align_val_up`]: self::align_val_up
+/// [`align_up`]: self::align_up
+#[inline(always)]
 pub const fn align_down(val: usize, order: usize) -> usize {
     let o = (1usize << order) - 1;
     val & !o
@@ -34,9 +42,10 @@ pub const fn align_down(val: usize, order: usize) -> usize {
 
 /// Returns the **aligned** value of `val`. Use the alignment of type `T`.
 ///
-/// This function always rounds up. See [`align_val_up`].
+/// This function always rounds up. See [`align_up`].
 ///
-/// [`align_val_up`]: self::align_val_up
+/// [`align_up`]: self::align_up
+#[inline(always)]
 pub const fn align_up_of<T>(val: usize) -> usize {
     // Type alignment is guaranteed be a power of 2.
     let order = align_of::<T>().trailing_zeros();
@@ -45,9 +54,10 @@ pub const fn align_up_of<T>(val: usize) -> usize {
 
 /// Returns the **aligned** value of `val`. Use the alignment of type `T`.
 ///
-/// This function always rounds down. See [`align_val_down`].
+/// This function always rounds down. See [`align_down`].
 ///
-/// [`align_val_down`]: self::align_val_down
+/// [`align_down`]: self::align_down
+#[inline(always)]
 pub const fn align_down_of<T>(val: usize) -> usize {
     let order = align_of::<T>().trailing_zeros();
     align_down(val, order as usize)
